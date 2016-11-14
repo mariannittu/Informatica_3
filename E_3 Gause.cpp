81fab1aa21e33b52cd41e4b988782413935dff1a
mariannittu <luca_000@hotmail.it>
2016-11-14 17:49:30 +0100

    Add files via upload

---
+++ E_3 Gause.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Morichetti Luca   3° INA   14/11/2016 */
+
+int main() {
+	 int N;// Valore dato
+	 printf("\n Inserire valore\n");
+	 scanf("%d", & N);
+	 N=(N+1)*(N/2);
+	 printf("\n La somma dei primi N numeri e'  %d",N);
+}
